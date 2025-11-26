0e0d665938b045ec596531f8dc0a3e02542f3e3d
Ayla Fazi <nisafazilatunalya@gmail.com>
2025-11-27 04:04:22 +0700

    percobaan 1

---
+++ Kafe06.java
@@ -0,0 +1,17 @@
+public class Kafe06 {
+   
+    public static void Menu (){
+        System.out.println("====== MENU RESTO CAFE ======");
+        System.out.println(" 1. Kopi hitam - Rp. 15,000 ");
+        System.out.println(" 2. Cappucino - Rp. 20,000 ");
+        System.out.println(" 3. Latte - Rp.22,000");
+        System.out.println(" 4. Teh tarik - Rp.12,000");
+        System.out.println(" 5. Roti bakar - Rp. 10.000");
+        System.out.println(" 6. Mie foreng - Rp. 18,000");
+        System.out.println(" ============================== ");
+        System.out.println(" Silahkan pilih menu yang anda inginkan. ");
+    }
+public static void main(String[] args) {
+    Menu();
+}
+}
