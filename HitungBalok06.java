2ac64cf96c732c1c8ec227d9911345050a9712ab
Ayla Fazi <nisafazilatunalya@gmail.com>
2025-11-29 07:41:16 +0700

    percobaan 5 fungsi

--- HitungBalok06.java
+++ HitungBalok06.java
@@ -1,6 +1,16 @@
 import java.util.Scanner;
 public class HitungBalok06 {
     
+     static int hitungLuas (int pjg, int lb){
+        int Luas = pjg*lb;
+        return Luas;
+    }
+
+    static int hitungVolume ( int tinggi, int a, int b){
+        int volume = hitungLuas(a, b)*tinggi;
+        return volume;
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int p,l,t,L, vol;
@@ -12,9 +22,10 @@ public class HitungBalok06 {
         System.out.print(" Masukkan tinggi: ");
         t = sc.nextInt();
        
-        L = p*l;
+        //pemanggilan fungsi
+        L = hitungLuas(p, l);
         System.out.println(" Luas persegi panjang: " + L);
-        vol = p*l*t;
+        vol = hitungVolume(t, p, l);
         System.out.println(" Volume balok adalah: " + vol);
 
         sc.close();
